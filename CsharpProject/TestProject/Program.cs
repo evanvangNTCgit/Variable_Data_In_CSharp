09859080609a06f098018603af30f938d8940867
Evan Vang <evankvang@gmail.com>
2025-06-04 16:41:46 -0500

    End of lesson 3

--- CsharpProject/TestProject/Program.cs
+++ CsharpProject/TestProject/Program.cs
@@ -1,37 +1,15 @@
-﻿/*
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-string message = "";
-decimal total = 0;
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
-foreach (var str in values)
+string[] orderList = orderStream.Split(",");
+
+foreach (var str in orderList)
 {
-    decimal currentstr;
-    if (decimal.TryParse(str, out currentstr))
+    if (str.Length != 4)
     {
-        total += currentstr;
+        System.Console.WriteLine($"{str} -- Error");
     }
     else
     {
-        message += str;
+        System.Console.WriteLine(str);
     }
 }
-
-System.Console.WriteLine($"\n Message: {message} \n Total: {total}");
-*/
-
-int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-int result1 = (int)Math.Round(value1 / value2);
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-decimal result2 = value2 / (decimal)value3;
-// Your code here to set result2
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-float result3 = value3 / (float)value1;
-// Your code here to set result3
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
