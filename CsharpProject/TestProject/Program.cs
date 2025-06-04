cd069286b7b03589e228735a3d11578dc6fb333f
Evan Vang <evankvang@gmail.com>
2025-06-04 14:46:28 -0500

    End of Lesson 2

--- CsharpProject/TestProject/Program.cs
+++ CsharpProject/TestProject/Program.cs
@@ -1,38 +1,37 @@
 ﻿/*
-Console.WriteLine("Signed integral types:");
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string message = "";
+decimal total = 0;
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+foreach (var str in values)
+{
+    decimal currentstr;
+    if (decimal.TryParse(str, out currentstr))
+    {
+        total += currentstr;
+    }
+    else
+    {
+        message += str;
+    }
+}
+
+System.Console.WriteLine($"\n Message: {message} \n Total: {total}");
 */
 
-/*
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
-*/
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-/*
-string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString);
-*/
-/*
-Value Type: Stores the actual data directly on the stack. 
-            Each variable gets its own copy of the data.
+int result1 = (int)Math.Round(value1 / value2);
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-Reference Type: Stores a reference (memory address) on the stack, 
-                which points to an object instance located on the heap. 
-                The heap is managed in RAM, and allocation is handled by the .NET runtime and OS.
-*/
+decimal result2 = value2 / (decimal)value3;
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-
-int val_A = 2;
-int val_B = val_A;
-val_B = 5;
-
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}");
+float result3 = value3 / (float)value1;
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
