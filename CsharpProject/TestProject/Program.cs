49d3fae18b08b41d808c8a38cedd897e6434838d
Evan Vang <evankvang@gmail.com>
2025-06-04 11:47:10 -0500

    End of lesson one

--- CsharpProject/TestProject/Program.cs
+++ CsharpProject/TestProject/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+*/
+
+/*
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+
+/*
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
+*/
+/*
+Value Type: Stores the actual data directly on the stack. 
+            Each variable gets its own copy of the data.
+
+Reference Type: Stores a reference (memory address) on the stack, 
+                which points to an object instance located on the heap. 
+                The heap is managed in RAM, and allocation is handled by the .NET runtime and OS.
+*/
+
+
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
