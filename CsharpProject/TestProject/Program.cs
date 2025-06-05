020242a926503a6e9ce00ea87fc5a182eee6fa94
Evan Vang <evankvang@gmail.com>
2025-06-04 20:23:48 -0500

    End of lesson 4

--- CsharpProject/TestProject/Program.cs
+++ CsharpProject/TestProject/Program.cs
@@ -1,15 +1,38 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+﻿/*
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
 
-string[] orderList = orderStream.Split(",");
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
 
-foreach (var str in orderList)
-{
-    if (str.Length != 4)
-    {
-        System.Console.WriteLine($"{str} -- Error");
-    }
-    else
-    {
-        System.Console.WriteLine(str);
-    }
-}
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine); */
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+System.Console.WriteLine("Dear {1} \nAs a customer of our {0} offering we are excited to tell you about a new financial product that would dramataically increase your return.", currentProduct, customerName);
+System.Console.WriteLine(($"\nCurrently, you own {currentShares} shares at a return of {currentReturn:P} \n Our new product, {newProduct} offers a return of {newReturn:P} given your current volume, your potential profit would be {newProfit:C}"));
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+comparisonMessage += $"{currentProduct.PadRight(20)}{currentReturn:P}  {currentProfit:c}\n";
+comparisonMessage += $"{newProduct.PadRight(20)}{newReturn:P}  {newProfit:c}";
+
+Console.WriteLine(comparisonMessage);
--- CsharpProject/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProject/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd069286b7b03589e228735a3d11578dc6fb333f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09859080609a06f098018603af30f938d8940867")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
